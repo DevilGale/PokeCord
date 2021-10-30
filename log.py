b79c4a721171731dadf9e0bf9e4c10dec203ba91
xharris <xhh950@gmail.com>
2021-10-30 03:46:55 -0400

    Revert "adds vscode workspace settings"
    This reverts commit 14d71ae2a5d7937e16000b849408ebc49cf231f5.

--- log.py
+++ log.py
@@ -1,45 +1,36 @@
 from datetime import datetime
 from enum import Enum
 
-
 class LEVEL(Enum):
-    INFO = 1
-    ERROR = 2
-    WARN = 3
-    DEBUG = 4
-
-    def __ge__(self, other):
-        if self.__class__ is other.__class__:
-            return self.value >= other.value
-        return NotImplemented
-
-    def __gt__(self, other):
-        if self.__class__ is other.__class__:
-            return self.value > other.value
-        return NotImplemented
-
-    def __le__(self, other):
-        if self.__class__ is other.__class__:
-            return self.value <= other.value
-        return NotImplemented
-
-    def __lt__(self, other):
-        if self.__class__ is other.__class__:
-            return self.value < other.value
-        return NotImplemented
-
+  INFO = 1
+  ERROR = 2
+  WARN = 3
+  DEBUG = 4
+  def __ge__(self, other):
+    if self.__class__ is other.__class__:
+      return self.value >= other.value
+    return NotImplemented
+  def __gt__(self, other):
+    if self.__class__ is other.__class__:
+      return self.value > other.value
+    return NotImplemented
+  def __le__(self, other):
+    if self.__class__ is other.__class__:
+      return self.value <= other.value
+    return NotImplemented
+  def __lt__(self, other):
+    if self.__class__ is other.__class__:
+      return self.value < other.value
+    return NotImplemented
 
 level = LEVEL.DEBUG
 
-
 def separator():
-    print("~~~~~~~~~~~~")
-
+  print('~~~~~~~~~~~~')
 
 def info(*args, **kwargs):
-    if level >= LEVEL.INFO:
-        print(f"[{datetime.now().strftime('%b-%d %H:%M')}] INFO", *args, **kwargs)
-
+  if level >= LEVEL.INFO:
+    print(f"[{datetime.now().strftime('%b-%d %H:%M')}] INFO",*args,**kwargs)
 
 # def error()
 # def warn()
